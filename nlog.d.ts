bf9099da690f092f1d11ad1ccb371f10dede8988
Tjakka5 <justinvanderleij@gmail.com>
2020-07-05 17:47:03 +0200

    Use unknown type instead of any

--- nlog.d.ts
+++ nlog.d.ts
@@ -3,16 +3,16 @@
 export function getLevel(): number;
 
 export function info(tag: string, text: string): void;
-export function infof(tag: string, text: string, ...args: any[]): void;
+export function infof(tag: string, text: string, ...args: unknown[]): void;
 
 export function warn(tag: string, text: string): void;
-export function warnf(tag: string, text: string, ...args: any[]): void;
+export function warnf(tag: string, text: string, ...args: unknown[]): void;
 
 export function warning(tag: string, text: string): void;
-export function warningf(tag: string, text: string, ...args: any[]): void;
+export function warningf(tag: string, text: string, ...args: unknown[]): void;
 
 export function error(tag: string, text: string): void;
-export function errorf(tag: string, text: string, ...args: any[]): void;
+export function errorf(tag: string, text: string, ...args: unknown[]): void;
 
 export function debug(tag: string, text: string): void;
-export function debugf(tag: string, text: string, ...args: any[]): void;
+export function debugf(tag: string, text: string, ...args: unknown[]): void;
