92b440bf35f6582c6aaf4c445ab8c9d1be2bce1f
Tjakka5 <justinvanderleij@gmail.com>
2020-07-05 16:48:40 +0200

    Add Typescript declarations for nlog

---
+++ nlog.d.ts
@@ -0,0 +1,18 @@
+/** @noSelfInFile */
+
+export function getLevel(): number;
+
+export function info(tag: string, text: string): void;
+export function infof(tag: string, text: string, ...args: any[]): void;
+
+export function warn(tag: string, text: string): void;
+export function warnf(tag: string, text: string, ...args: any[]): void;
+
+export function warning(tag: string, text: string): void;
+export function warningf(tag: string, text: string, ...args: any[]): void;
+
+export function error(tag: string, text: string): void;
+export function errorf(tag: string, text: string, ...args: any[]): void;
+
+export function debug(tag: string, text: string): void;
+export function debugf(tag: string, text: string, ...args: any[]): void;
